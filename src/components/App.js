649294ee105936282f36d94906db9d1f29449ec9
tonniegeetz <tonniegeetz@gmail.com>
2024-06-05 22:35:35 +0300

    routes understood

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,30 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
-import Movies from "./Movies";
+import Movies from './Movies'
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <NavBar />
+    <Switch>
+      <Route exact path='/' >
+        <Home />
+      </Route>
+
+      <Route exact path='/movies' >
+        <Movies />
+      </Route>
+
+      <Route exact path='/directors' >
+        <Directors />
+      </Route>
+
+      <Route path='/actors' >
+        <Actors />
+      </Route>
+
+    </Switch>
+  </div>;
 }
 
 export default App;
